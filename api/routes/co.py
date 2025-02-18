a73eb1faf7df72d4b0a1a46e53f2cd002b536abb
Aayushi <emailaayushi15@gmail.com>
2025-02-17 19:39:09 -0500

    Delete api directory

--- api/routes/co.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-co_bp = Blueprint('co', __name__)
-
-@co_bp.route('/')
-def get_county():
-    return "County data"
--- api/routes/no2.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-no2_bp = Blueprint('no2', __name__)
-
-@no2_bp.route('/')
-def get_county():
-    return "County data"
